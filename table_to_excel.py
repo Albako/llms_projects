b6401c93e9d58351af9a6bd222e48cd91f880a81
Albako <170086410+Albako@users.noreply.github.com>
2025-04-15 11:41:50 +0200

    Add files via upload

--- table_to_excel.py
+++ table_to_excel.py
@@ -1,25 +1,28 @@
 from openpyxl import Workbook
-markdown_table = """
-| Name    | Age | Occupation    | Favorite Hobby   |
-|---------|-----|---------------|------------------|
-| Alice   | 28  | Engineer      | Paiting          |
-| Bob     | 35  | Doctor        | Cycling          |
-| Charlie | 42  | Teacher       | Hiking           |
-| David   | 30  | Photographer  | Chess            |"""
 
-rows = markdown_table.split("\n")
-spreadsheet=[]
-for index,row in enumerate(rows):
-    if index==1:
-        continue
-    split_rows = row.split("|")
-    spreadsheet.append(split_rows[1:-1]) # skip first & last element
+# markdown_table = """
+# | Name    | Age | Occupation    | Favorite Hobby   |
+# |---------|-----|---------------|------------------|
+# | Alice   | 28  | Engineer      | Paiting          |
+# | Bob     | 35  | Doctor        | Cycling          |
+# | Charlie | 42  | Teacher       | Hiking           |
+# | David   | 30  | Photographer  | Chess            |"""
 
-wb = Workbook()
-ws = wb.active
+def create_excel(markdown_table, excel_file_name):
 
-for row in spreadsheet:
-    ws.append(row)
-    
-wb.save("table_to_excle.xlsx")
-print("Table saved succsesfully")
+    rows = markdown_table.split("\n")
+    spreadsheet=[]
+    for index,row in enumerate(rows):
+        if index==1:
+            continue
+        split_rows = row.split("|")
+        spreadsheet.append(split_rows[1:-1]) # skip first & last element
+
+    wb = Workbook()
+    ws = wb.active
+
+    for row in spreadsheet:
+        ws.append(row)
+        
+    wb.save(f"{excel_file_name}.xlsx")
+    print("Table saved succsesfully")
